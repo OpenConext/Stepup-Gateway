30c2bfd28371575360016c1ae316483277d87397
Reinier Kip <reinier@ibuildings.nl>
2014-10-20 16:43:40 +0200

    Implement JsonConvertibleParamConverter.

---
+++ src/Surfnet/StepupBundle/Exception/BadRequestException.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+ * Copyright 2014 SURFnet bv
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Surfnet\StepupBundle\Exception;
+
+/**
+ * Thrown when a client provided invalid input to the application.
+ */
+class BadRequestException extends \DomainException
+{
+    /**
+     * @var string[]
+     */
+    private $errors;
+
+    /**
+     * @param string $message
+     * @param string[] $errors
+     * @param int $code
+     * @param \Exception $previous
+     */
+    public function __construct($message, array $errors, $code = 0, \Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+
+        $this->errors = $errors;
+    }
+
+    /**
+     * @return string[]
+     */
+    public function getErrors()
+    {
+        return $this->errors;
+    }
+}
---
+++ src/Surfnet/StepupBundle/Request/JsonConvertibleParamConverter.php
@@ -0,0 +1,125 @@
+<?php
+
+/**
+ * Copyright 2014 SURFnet bv
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Surfnet\StepupBundle\Request;
+
+use ReflectionClass;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
+use Sensio\Bundle\FrameworkExtraBundle\Request\ParamConverter\ParamConverterInterface;
+use Surfnet\StepupBundle\Exception\BadRequestException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Validator\ConstraintViolation;
+use Symfony\Component\Validator\ConstraintViolationInterface;
+use Symfony\Component\Validator\ConstraintViolationListInterface;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+
+/**
+ * ParamConverter that converts JSON objects with underscore notation mapped to snake-cased, public properties of
+ * classes that implement JsonConvertible.
+ *
+ * @see JsonConvertible
+ */
+class JsonConvertibleParamConverter implements ParamConverterInterface
+{
+    /**
+     * @var ValidatorInterface
+     */
+    private $validator;
+
+    public function __construct(ValidatorInterface $validator)
+    {
+        $this->validator = $validator;
+    }
+
+    public function apply(Request $request, ParamConverter $configuration)
+    {
+        $name = $configuration->getName();
+        $class = $configuration->getClass();
+
+        $json = $request->getContent();
+        $object = json_decode($json, true);
+
+        if (isset($object[$name]) && is_array($object[$name])) {
+            $object = $object[$name];
+        } else {
+            $object = [];
+        }
+
+        $convertedObject = new $class;
+
+        $unknownProperties = [];
+
+        foreach ($object as $key => $value) {
+            $properlyCasedKey = lcfirst(str_replace(' ', '', ucwords(str_replace('_', ' ', $key))));
+
+            if (!property_exists($convertedObject, $properlyCasedKey)) {
+                $unknownProperties[$properlyCasedKey] = $value;
+
+                continue;
+            }
+
+            $convertedObject->$properlyCasedKey = $value;
+        }
+
+        $violations = $this->validator->validate($convertedObject);
+
+        foreach ($unknownProperties as $property => $value) {
+            $message = sprintf("Unknown property '%s'", $unknownProperties);
+            $violation = new ConstraintViolation($message, $message, [], $convertedObject, $property, $value);
+
+            $violations->add($violation);
+        }
+
+        if ($violations->count() > 0) {
+            throw new BadRequestException(
+                'JSON could not be reconstituted into valid object.',
+                $this->mapViolationsToErrorStrings($violations, $name)
+            );
+        }
+
+        $request->attributes->set($name, $convertedObject);
+    }
+
+    public function supports(ParamConverter $configuration)
+    {
+        $class = $configuration->getClass();
+
+        if (!is_string($class)) {
+            return null;
+        }
+
+        return (new ReflectionClass($class))->implementsInterface(__NAMESPACE__ . '\\JsonConvertible');
+    }
+
+    /**
+     * @param ConstraintViolationListInterface $violations
+     * @param string $root
+     * @return array
+     */
+    private function mapViolationsToErrorStrings(ConstraintViolationListInterface $violations, $root)
+    {
+        $errors = [];
+
+        foreach ($violations as $violation) {
+            /** @var ConstraintViolationInterface $violation */
+            $errors[] = sprintf('%s.%s: %s', $root, $violation->getPropertyPath(), $violation->getMessage());
+        }
+
+        return $errors;
+    }
+}
