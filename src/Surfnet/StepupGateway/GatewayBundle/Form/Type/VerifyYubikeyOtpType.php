2c9675332a1096b7bd98f9bc4d9e31974cfbff0b
Reinier Kip <reinier@ibuildings.nl>
2014-12-17 12:12:20 +0100

    Configure MopaBootstrapBundle.

--- src/Surfnet/StepupGateway/GatewayBundle/Form/Type/VerifyYubikeyOtpType.php
+++ src/Surfnet/StepupGateway/GatewayBundle/Form/Type/VerifyYubikeyOtpType.php
@@ -22,7 +22,7 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolverInterface;
 
-class ProveYubikeyPossessionType extends AbstractType
+class VerifyYubikeyOtpType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
