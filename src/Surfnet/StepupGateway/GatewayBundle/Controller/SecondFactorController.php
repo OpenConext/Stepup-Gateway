OpenConext/Stepup-Gateway

commit 94080f2bd6d375941a37f676802e3afc8c11bf4d

Assert SHO values are valid
By adding an assertion to the StepUpAuthenticationService, we gained
the possibility to validate the SHO value. A valid SHO value is a
lower cased (or empty) string. When an invalid SHO is tested, an
InvalidStepupShoFormatException is raised. Tests where added to validate
the correct working of the method.

In this commit the previous SHO normalization was undone.

(cherry picked from commit 2afb182)

src/Surfnet/StepupGateway/GatewayBundle/Controller/SecondFactorController.php
@@ -58,6 +58,9 @@ class SecondFactorController extends Controller
             $idpSho = $context->getSchacHomeOrganization();
             $userSho = $this->getStepupService()->getUserShoByIdentityNameId($context->getIdentityNameId());
 
+            $this->getStepupService()->assertValidShoFormat($idpSho);
+            $this->getStepupService()->assertValidShoFormat($userSho);
+
             $requiredLoa = $this
                 ->getStepupService()
                 ->resolveHighestRequiredLoa(

src/Surfnet/StepupGateway/GatewayBundle/Service/StepUpAuthenticationService.php
@@ -38,6 +38,7 @@ use Surfnet\StepupGateway\GatewayBundle\Command\VerifyYubikeyOtpCommand;
 use Surfnet\StepupGateway\GatewayBundle\Entity\SecondFactor;
 use Surfnet\StepupGateway\GatewayBundle\Entity\SecondFactorRepository;
 use Surfnet\StepupGateway\GatewayBundle\Exception\InstitutionMismatchException;
+use Surfnet\StepupGateway\GatewayBundle\Exception\InvalidStepupShoFormatException;
 use Surfnet\StepupGateway\GatewayBundle\Exception\LoaCannotBeGivenException;
 use Surfnet\StepupGateway\GatewayBundle\Exception\UnknownInstitutionException;
 use Surfnet\StepupGateway\GatewayBundle\Service\StepUp\YubikeyOtpVerificationResult;
@@ -406,13 +407,33 @@ class StepUpAuthenticationService
     }
 
     /**
-     * Get the normalized (lowercase) schacHomeOrganisation of the authenticating user based on its vetted tokens.
+     * Get the schacHomeOrganisation of the authenticating user based on its vetted tokens.
      *
      * @param string $identityNameId Used to load vetted tokens
      * @return string either the SHO or an empty string
      */
     public function getUserShoByIdentityNameId($identityNameId)
     {
-        return strtolower($this->secondFactorRepository->getInstitutionByNameId($identityNameId));
+        return $this->secondFactorRepository->getInstitutionByNameId($identityNameId);
+    }
+
+    /**
+     * In Stepup, the schacHomeOrganization must be in a lower case format. Please note that empty string values are
+     * considered valid SHO in this method.
+     *
+     * @param string $schacHomeOrganization
+     * @throws InvalidStepupShoFormatException Raised when the SHO is not compatible with the Stepup standard (lower
+     *                                         cased string)
+     */
+    public function assertValidShoFormat($schacHomeOrganization)
+    {
+        if (!empty($schacHomeOrganization) && strtolower($schacHomeOrganization) !== $schacHomeOrganization) {
+            throw new InvalidStepupShoFormatException(
+                sprintf(
+                    'Encountered an invalid schacHomeOrganization value "%s".',
+                    $schacHomeOrganization
+                )
+            );
+        }
     }
 }

src/Surfnet/StepupGateway/GatewayBundle/Tests/Service/StepUpAuthenticationServiceTest.php
@@ -28,8 +28,8 @@ use Surfnet\StepupBundle\Service\SmsSecondFactorService;
 use Surfnet\StepupBundle\Value\Loa;
 use Surfnet\StepupGateway\ApiBundle\Service\YubikeyService;
 use Surfnet\StepupGateway\GatewayBundle\Entity\SecondFactorRepository;
+use Surfnet\StepupGateway\GatewayBundle\Exception\InvalidStepupShoFormatException;
 use Surfnet\StepupGateway\GatewayBundle\Exception\LoaCannotBeGivenException;
-use Surfnet\StepupGateway\GatewayBundle\Exception\RuntimeException;
 use Surfnet\StepupGateway\GatewayBundle\Service\InstitutionMatchingHelper;
 use Surfnet\StepupGateway\GatewayBundle\Service\StepUpAuthenticationService;
 use Symfony\Component\Translation\TranslatorInterface;
@@ -492,4 +492,37 @@ final class StepUpAuthenticationServiceTest extends PHPUnit_Framework_TestCase
 
         return $combinations;
     }
+
+    public function test_assert_valid_sho()
+    {
+        $result = $this->service->assertValidShoFormat('valid.sho');
+        $this->assertNull($result);
+
+        $result = $this->service->assertValidShoFormat('');
+        $this->assertNull($result);
+    }
+
+    /**
+     * @dataProvider invalidShoProvider
+     * @param $invalidSho
+     */
+    public function test_assert_invalid_sho($invalidSho)
+    {
+        $this->setExpectedException(
+            InvalidStepupShoFormatException::class,
+            sprintf('Encountered an invalid schacHomeOrganization value "%s".', $invalidSho)
+        );
+        $this->service->assertValidShoFormat($invalidSho);
+    }
+
+    public function invalidShoProvider()
+    {
+        return [
+            ['INVALID'],
+            ['iNvAlId'],
+            ['iNvÄlId'],
+            ['in.valiD'],
+        ];
+    }
+
 }

src/Surfnet/StepupGateway/GatewayBundle/Exception/InvalidStepupShoFormatException.php
@@ -1,0 +1,23 @@
+<?php
+
+/**
+ * Copyright 2018 SURFnet bv
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace Surfnet\StepupGateway\GatewayBundle\Exception;
+
+class InvalidStepupShoFormatException extends LoaCannotBeGivenException
+{
+}

commit 1625b4027cdc44d6ef23ac0a861e0dd51e629d47

Allow mixed-case schacHomeOrganization values
Commit 94080f2 introduced behaviour to the gateway were an error was
thrown on mixed-case SHO values. This behaviour is reverted in this
commit: mixed-case SHO is allowed and compared in an case-insensitive
manner.

While the case of SHO values of the SP/IDP no longer matters for
StepUp, it is important for the SP configuration (SP-configured LoAs)
to always have lower-cased SHOs.

See https://www.pivotaltracker.com/story/show/154845486

src/Surfnet/StepupGateway/GatewayBundle/Controller/SecondFactorController.php
@@ -58,19 +58,17 @@ class SecondFactorController extends Controller
             // Retrieve all requirements to determine the required LoA
             $requestedLoa = $context->getRequiredLoa();
             $spConfiguredLoas = $context->getServiceProvider()->get('configuredLoas');
-            $idpSho = $context->getSchacHomeOrganization();
-            $userSho = $this->getStepupService()->getUserShoByIdentityNameId($context->getIdentityNameId());
 
-            $this->getStepupService()->assertValidShoFormat($idpSho);
-            $this->getStepupService()->assertValidShoFormat($userSho);
+            $normalizedIdpSho = $context->getNormalizedSchacHomeOrganization();
+            $normalizedUserSho = $this->getStepupService()->getNormalizedUserShoByIdentityNameId($context->getIdentityNameId());
 
             $requiredLoa = $this
                 ->getStepupService()
                 ->resolveHighestRequiredLoa(
                     $requestedLoa,
                     $spConfiguredLoas,
-                    $idpSho,
-                    $userSho
+                    $normalizedIdpSho,
+                    $normalizedUserSho
                 );
         } catch (LoaCannotBeGivenException $e) {
             // Log the message of the domain exception, this contains a meaningful message.

src/Surfnet/StepupGateway/GatewayBundle/Saml/ResponseContext.php
@@ -202,9 +202,22 @@ class ResponseContext
         return $this->stateHandler->getIdentityNameId();
     }
 
-    public function getSchacHomeOrganization()
+    /**
+     * Return the lower-cased schacHomeOrganization value from the assertion.
+     *
+     * Comparisons on SHO values should always be case insensitive. Stepup
+     * configuration always contains SHO values lower-cased, so this getter
+     * can be used to compare the SHO with configured values.
+     *
+     * @see StepUpAuthenticationService::resolveHighestRequiredLoa()
+     *
+     * @return null|string
+     */
+    public function getNormalizedSchacHomeOrganization()
     {
-        return $this->stateHandler->getSchacHomeOrganization();
+        return strtolower(
+            $this->stateHandler->getSchacHomeOrganization()
+        );
     }
 
     /**

src/Surfnet/StepupGateway/GatewayBundle/Service/StepUpAuthenticationService.php
@@ -180,14 +180,14 @@ class StepUpAuthenticationService
      *                                                'institution_b' => 'https://example.com/authentication/loa3',
      *                                            ]
      *
-     * @param string $idpSho           <IdP-SHO>  Optional. Value of the schacHomeOrganization attribute from the
+     * @param string $normalizedIdpSho            <IdP-SHO>  Optional. Value of the schacHomeOrganization attribute from the
      *                                            Assertion from the IdP. The SHO should be normalized (lower cased) as
      *                                            it will be used to be compared against the $spConfiguredLoas who have
      *                                            also been normalized.
      *
      *                                            Example: 'institution_a', ''
      *
-     * @param string $userSho          <User-SHO> Optional. The schacHomeOrganization that the user belongs to, this is
+     * @param string $normalizedUserSho           <User-SHO> Optional. The schacHomeOrganization that the user belongs to, this is
      *                                            the schacHomeOrganization that was provided during registration of the
      *                                            token. The SHO should be normalized (lower cased) as it will be used
      *                                            to be compared against the $spConfiguredLoas who have also been
@@ -213,8 +213,8 @@ class StepUpAuthenticationService
     public function resolveHighestRequiredLoa(
         $requestedLoa,
         array $spConfiguredLoas,
-        $idpSho,
-        $userSho
+        $normalizedIdpSho,
+        $normalizedUserSho
     ) {
         // Candidate LoA's are stored in a collection. At the end of this procedure, the highest LoA is selected from
         // this collection.
@@ -236,25 +236,25 @@ class StepUpAuthenticationService
 
         if ($this->hasNonDefaultSpConfiguredLoas($spConfiguredLoas)) {
             // We need an userSho or idpSho to determine if any of the <LoAs> are applicable
-            if (empty($userSho) && empty($idpSho)) {
+            if (empty($normalizedUserSho) && empty($normalizedIdpSho)) {
                 throw new UnknownInstitutionException('Unable to determine the institution for authenticating user.');
             }
 
             // If both user and IdP SHO are known, they should match
-            if (!empty($userSho) && !empty($idpSho) && $userSho != $idpSho) {
+            if (!empty($normalizedUserSho) && !empty($normalizedIdpSho) && $normalizedUserSho != $normalizedIdpSho) {
                 throw new InstitutionMismatchException('User and IdP SHO are set but do not match.');
             }
 
-            // If the user SHO is available in the <LoAs>, add the to the candidates collection.
-            if (isset($spConfiguredLoas[$userSho])) {
+            // If the user SHO is available in the <LoAs>, add to the candidates collection.
+            if (isset($spConfiguredLoas[$normalizedUserSho])) {
                 $this->logger->info(sprintf('Added Loa "%s" as candidate based on user SHO', $requestedLoa));
-                $loaCandidates->add($spConfiguredLoas[$userSho]);
+                $loaCandidates->add($spConfiguredLoas[$normalizedUserSho]);
             }
 
             // If the IdP SHO is available in the <LoAs>, add the to the candidates collection.
-            if (isset($spConfiguredLoas[$idpSho])) {
+            if (isset($spConfiguredLoas[$normalizedIdpSho])) {
                 $this->logger->info(sprintf('Added Loa "%s" as candidate based on IdP SHO', $requestedLoa));
-                $loaCandidates->add($spConfiguredLoas[$idpSho]);
+                $loaCandidates->add($spConfiguredLoas[$normalizedIdpSho]);
             }
         }
 
@@ -420,33 +420,21 @@ class StepUpAuthenticationService
     }
 
     /**
-     * Get the schacHomeOrganisation of the authenticating user based on its vetted tokens.
+     * Return the lower-cased schacHomeOrganization of the user based on his vetted tokens.
+     *
+     * Comparisons on SHO values should always be case insensitive. Stepup
+     * configuration always contains SHO values lower-cased, so this getter
+     * can be used to compare the SHO with configured values.
+     *
+     * @see StepUpAuthenticationService::resolveHighestRequiredLoa()
      *
      * @param string $identityNameId Used to load vetted tokens
      * @return string either the SHO or an empty string
      */
-    public function getUserShoByIdentityNameId($identityNameId)
+    public function getNormalizedUserShoByIdentityNameId($identityNameId)
     {
-        return $this->secondFactorRepository->getInstitutionByNameId($identityNameId);
-    }
-
-    /**
-     * In Stepup, the schacHomeOrganization must be in a lower case format. Please note that empty string values are
-     * considered valid SHO in this method.
-     *
-     * @param string $schacHomeOrganization
-     * @throws InvalidStepupShoFormatException Raised when the SHO is not compatible with the Stepup standard (lower
-     *                                         cased string)
-     */
-    public function assertValidShoFormat($schacHomeOrganization)
-    {
-        if (!empty($schacHomeOrganization) && strtolower($schacHomeOrganization) !== $schacHomeOrganization) {
-            throw new InvalidStepupShoFormatException(
-                sprintf(
-                    'Encountered an invalid schacHomeOrganization value "%s".',
-                    $schacHomeOrganization
-                )
-            );
-        }
+        return strtolower(
+            $this->secondFactorRepository->getInstitutionByNameId($identityNameId)
+        );
     }
 }

src/Surfnet/StepupGateway/GatewayBundle/Tests/Service/StepUpAuthenticationServiceTest.php
@@ -479,36 +479,4 @@ final class StepUpAuthenticationServiceTest extends PHPUnit_Framework_TestCase
         return $combinations;
     }
 
-    public function test_assert_valid_sho()
-    {
-        $result = $this->service->assertValidShoFormat('valid.sho');
-        $this->assertNull($result);
-
-        $result = $this->service->assertValidShoFormat('');
-        $this->assertNull($result);
-    }
-
-    /**
-     * @dataProvider invalidShoProvider
-     * @param $invalidSho
-     */
-    public function test_assert_invalid_sho($invalidSho)
-    {
-        $this->setExpectedException(
-            InvalidStepupShoFormatException::class,
-            sprintf('Encountered an invalid schacHomeOrganization value "%s".', $invalidSho)
-        );
-        $this->service->assertValidShoFormat($invalidSho);
-    }
-
-    public function invalidShoProvider()
-    {
-        return [
-            ['INVALID'],
-            ['iNvAlId'],
-            ['iNvÄlId'],
-            ['in.valiD'],
-        ];
-    }
-
 }