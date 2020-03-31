7cf7d89154ca2fed996e464d2deafa6e985300c4
Bas Strooband <bstrooband@ibuildings.nl>
2020-03-31 12:29:44 +0200

    Fix config 'webtest'

--- ci/docker/init.sh
+++ ci/docker/init.sh
@@ -7,11 +7,6 @@ printf "UID=${uid}\nGID=${gid}\nCOMPOSE_PROJECT_NAME=gateway" > .env
 docker-compose up -d --build
 
 docker-compose exec -T php-fpm.stepup.example.com bash -c '
-  cp ./ci/config/*.yml ./app/config/
-  cp ./ci/certificates/* ./app/
-  cp ./ci/app.php ./web/app.php
-  composer install --prefer-dist -n -o --no-scripts && \
-  composer distribution-bundle-scripts && \
-  ./app/console mopa:bootstrap:symlink:less --env=test && \
-  ./app/console assetic:dump --env=test --verbose
-'
+  composer install --prefer-dist -n -o && \
+  ./app/console mopa:bootstrap:symlink:less --env=webtest && \
+  ./app/console assetic:dump --env=webtest --verbose'
