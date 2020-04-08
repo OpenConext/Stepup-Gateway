61b5cc8708658c492da25075f9e0bc3062fabdf9
Michiel Kodde <mkodde@ibuildings.nl>
2020-04-08 15:22:28 +0200

    Resolve conflict in init.sh

--- ci/docker/init.sh
+++ ci/docker/init.sh
@@ -4,13 +4,9 @@ gid=$(id -g)
 
 printf "UID=${uid}\nGID=${gid}\nCOMPOSE_PROJECT_NAME=gateway" > .env
 
-docker-compose up -d
+docker-compose up -d --build
 
 docker-compose exec -T php-fpm.stepup.example.com bash -c '
-  cp ./ci/config/*.yml ./app/config/
-  cp ./ci/certificates/* ./app/
-  composer install --prefer-dist -n -o --no-scripts && \
-  composer distribution-bundle-scripts && \
-  ./app/console mopa:bootstrap:symlink:less --env=test && \
-  ./app/console assetic:dump --env=test --verbose
-'
+  composer install --prefer-dist -n -o && \
+  ./app/console mopa:bootstrap:symlink:less --env=webtest && \
+  ./app/console assetic:dump --env=webtest --verbose'
