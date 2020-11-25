216dddc4ef257a4b3695dfbcbd3a01f5b20542c1
Martin Roest <roest.martin@gmail.com>
2020-11-25 12:28:50 +0100

    Update github workflow/ci

--- ci/docker/init.sh
+++ ci/docker/init.sh
@@ -7,10 +7,10 @@ printf "UID=${uid}\nGID=${gid}\nCOMPOSE_PROJECT_NAME=gateway" > .env
 docker-compose up -d
 
 docker-compose exec -T php-fpm.stepup.example.com bash -c '
-  cp ./ci/config/*.yml ./app/config/
-  cp ./ci/certificates/* ./app/
+  cp ./ci/config/*.yaml ./config/legacy/ && \
+  mkdir -p app/files && \
+  cp ./ci/certificates/* ./app/files/ && \
   composer install --prefer-dist -n -o --no-scripts && \
   composer frontend-install && \
-  composer distribution-bundle-scripts && \
   ./app/console assets:install --env=test --verbose
 '
