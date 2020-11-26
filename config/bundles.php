042d86b46f824e5276b447d08c037f1231f8ab79
Martin Roest <roest.martin@gmail.com>
2020-11-26 16:29:41 +0100

    Remove unsed behat symfony extension

--- config/bundles.php
+++ config/bundles.php
@@ -25,5 +25,4 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
-    FriendsOfBehat\SymfonyExtension\Bundle\FriendsOfBehatSymfonyExtensionBundle::class => ['test' => true],
 ];
