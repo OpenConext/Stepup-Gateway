dcd26fcb9e11d577014b18b84780d2fd606cc4e5
Martin Roest <roest.martin@gmail.com>
2021-02-25 10:52:15 +0100

    Update SAML bundle and Symfony security scanner
    This updates the SAML bundle which is required for the extension
    support. The dependency update also requires the Symfony security
    scanner, which is EOL, to be removed and is updated to the newer
    local cli version.

--- config/bundles.php
+++ config/bundles.php
@@ -25,4 +25,5 @@ return [
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
