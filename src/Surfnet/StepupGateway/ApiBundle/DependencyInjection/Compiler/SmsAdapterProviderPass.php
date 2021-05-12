0c93dd43be1d9212c3cc69b10f39da7497f50c3b
Michiel Kodde <mkodde@ibuildings.nl>
2021-05-12 15:53:22 +0200

    Move a UnusedLocalvariables suppression

--- src/Surfnet/StepupGateway/ApiBundle/DependencyInjection/Compiler/SmsAdapterProviderPass.php
+++ src/Surfnet/StepupGateway/ApiBundle/DependencyInjection/Compiler/SmsAdapterProviderPass.php
@@ -22,6 +22,9 @@ use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
 
+/**
+ * @SuppressWarnings(PHPMD.UnusedLocalVariables)
+ */
 class SmsAdapterProviderPass implements CompilerPassInterface
 {
     public function process(ContainerBuilder $container): void
