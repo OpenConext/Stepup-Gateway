44eb9fa3b5aab79544ad5e92103e6d878c904a5f
Michiel Kodde <mkodde@ibuildings.nl>
2023-08-09 15:35:17 +0200

    Uninstall Doctrine Migrations
    Migrations are managed via Stepup-Middleware. Having them in this
    project makes no sense. And might cause confusion

--- config/bundles.php
+++ config/bundles.php
@@ -2,7 +2,6 @@
 
 return [
     Doctrine\Bundle\DoctrineBundle\DoctrineBundle::class => ['all' => true],
-    Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle::class => ['all' => true],
     JMS\TranslationBundle\JMSTranslationBundle::class => ['all' => true],
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
     Nelmio\SecurityBundle\NelmioSecurityBundle::class => ['all' => true],
