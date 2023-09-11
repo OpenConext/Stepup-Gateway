695ade283ddc3bece10eb300442a5faf9e2d08e1
Michiel Kodde <mkodde@ibuildings.nl>
2023-09-11 11:55:32 +0200

    Explain, the Second Factor ID is read from state

--- src/Surfnet/StepupGateway/GatewayBundle/Sso2fa/CookieService.php
+++ src/Surfnet/StepupGateway/GatewayBundle/Sso2fa/CookieService.php
@@ -102,6 +102,8 @@ class CookieService implements CookieServiceInterface
             );
             return $httpResponse;
         }
+
+        // The second factor id is read from state. It is the SF Id of the token used during authentication
         $secondFactorId = $responseContext->getSelectedSecondFactor();
 
         // We can only set an SSO on 2FA cookie if a second factor authentication is being handled.
