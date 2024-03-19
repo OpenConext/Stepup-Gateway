8b5ca4128cf74f1a2a1cf6ea76cb6b6c98f850d3
Paul Rijke <paul@ibuildings.nl>
2024-03-19 14:21:43 +0100

    Remove swiftmailer dependency

--- config/bundles.php
+++ config/bundles.php
@@ -17,7 +17,6 @@ return [
     Surfnet\SamlBundle\SurfnetSamlBundle::class => ['all' => true],
     Symfony\Bundle\MonologBundle\MonologBundle::class => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class => ['all' => true],
-    Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
