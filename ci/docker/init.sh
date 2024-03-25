481867c587585c930a4bec4c0318d22766ffaaf7
Bart Geesink <bart.geesink@surf.nl>
2024-03-25 15:15:19 +0100

    Integration tests: explicictly instantiate .dist files

--- ci/docker/init.sh
+++ ci/docker/init.sh
@@ -5,8 +5,11 @@ docker compose up gateway selenium haproxy ssp mariadb -d
 
 docker compose exec -T gateway bash -c '
   cp /var/www/html/devconf/stepup/gateway/surfnet_yubikey.yaml.dist /var/www/html/devconf/stepup/gateway/surfnet_yubikey.yaml && \
+  cp /var/www/html/config/legacy/parameters.yaml.dist /var/www/html/config/legacy/parameters.yaml && \
+  cp /var/www/html/config/legacy/samlstepupproviders_parameters.yaml.dist /var/www/html/config/legacy/samlstepupproviders_parameters.yaml && \
+  cp /var/www/html/config/legacy/global_view_parameters.yaml.dist /var/www/html/config/legacy/global_view_parameters.yaml && \
   composer install --prefer-dist -n -o --no-scripts && \
-  composer frontend-install && \
+  composer frontend-install && \global_view_parameters.yaml.dist
   ./bin/console assets:install --env=smoketest --verbose && \
   ./bin/console cache:clear --env=smoketest && \
   chown -R www-data:www-data /var/www/html/var/
