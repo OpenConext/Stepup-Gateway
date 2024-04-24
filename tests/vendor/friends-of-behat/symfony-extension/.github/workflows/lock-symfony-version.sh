29b99b22885feaaa634a613ed981588d02c2142e
Michiel Kodde <mkodde@ibuildings.nl>
2024-04-24 09:10:43 +0200

    Do not track tests/vendor

--- tests/vendor/friends-of-behat/symfony-extension/.github/workflows/lock-symfony-version.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-cat <<< $(jq --indent 4 --arg version $VERSION '.require |= with_entries(if (.key|test("^symfony/")) then .value=$version else . end)' < composer.json) > composer.json
-cat <<< $(jq --indent 4 --arg version $VERSION '."require-dev" |= with_entries(if (.key|test("^symfony/")) then .value=$version else . end)' < composer.json) > composer.json
