a145520ce8a418c82f44cd5cc32339ceb9c4813e
Michiel Kodde <mkodde@ibuildings.nl>
2024-05-15 11:08:54 +0200

    Update PHPStan baselne
    2 warnings where no longer valid

--- ci/qa/phpstan-baseline.php
+++ ci/qa/phpstan-baseline.php
@@ -2,25 +2,17 @@
 
 $ignoreErrors = [];
 $ignoreErrors[] = [
-	'message' => '#^Attribute class Surfnet\\\\StepupGateway\\\\ApiBundle\\\\Controller\\\\Route does not exist\\.$#',
-	'count' => 1,
-	'path' => __DIR__ . '/../../src/Surfnet/StepupGateway/ApiBundle/Controller/SmsController.php',
-];
-$ignoreErrors[] = [
+	// identifier: function.deprecated
 	'message' => '#^Call to deprecated function GuzzleHttp\\\\json_decode\\(\\)\\:
 json_decode will be removed in guzzlehttp/guzzle\\:8\\.0\\. Use Utils\\:\\:jsonDecode instead\\.$#',
 	'count' => 1,
 	'path' => __DIR__ . '/../../src/Surfnet/StepupGateway/GatewayBundle/Entity/SamlEntity.php',
 ];
 $ignoreErrors[] = [
+	// identifier: parameter.requiredAfterOptional
 	'message' => '#^Deprecated in PHP 8\\.0\\: Required parameter \\$value follows optional parameter \\$propertyPath\\.$#',
 	'count' => 1,
 	'path' => __DIR__ . '/../../src/Surfnet/StepupGateway/GatewayBundle/Exception/AssertionFailedException.php',
-];
-$ignoreErrors[] = [
-	'message' => '#^Call to an undefined method Surfnet\\\\StepupGateway\\\\SamlStepupProviderBundle\\\\Controller\\\\SamlProxyController\\:\\:getServiceProvider\\(\\)\\.$#',
-	'count' => 1,
-	'path' => __DIR__ . '/../../src/Surfnet/StepupGateway/SamlStepupProviderBundle/Controller/SamlProxyController.php',
 ];
 
 return ['parameters' => ['ignoreErrors' => $ignoreErrors]];
