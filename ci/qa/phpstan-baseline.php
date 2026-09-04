3dcd1c525442729855ebe73b7859c9b08590792f
Kay Joosten <kay.joosten@dawn.tech>
2026-09-04 11:02:25 +0200

    fix: use native json_decode instead of deprecated GuzzleHttp helpers
    GuzzleHttp\json_decode() and GuzzleHttp\Utils::jsonDecode() are both
    deprecated as of guzzle 7.15 and removed in 8.0. Use PHP's native
    json_decode() with JSON_THROW_ON_ERROR to preserve the throw-on-invalid
    behaviour. Drop the now-obsolete phpstan baseline ignore.

--- ci/qa/phpstan-baseline.php
+++ ci/qa/phpstan-baseline.php
@@ -1,12 +1,5 @@
 <?php declare(strict_types = 1);
 
 $ignoreErrors = [];
-$ignoreErrors[] = [
-	// identifier: function.deprecated
-	'message' => '#^Call to deprecated function GuzzleHttp\\\\json_decode\\(\\)\\:
-json_decode will be removed in guzzlehttp/guzzle\\:8\\.0\\. Use Utils\\:\\:jsonDecode instead\\.$#',
-	'count' => 1,
-	'path' => __DIR__ . '/../../src/Surfnet/StepupGateway/GatewayBundle/Entity/SamlEntity.php',
-];
 
 return ['parameters' => ['ignoreErrors' => $ignoreErrors]];
